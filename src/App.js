66d1d04c794da8c0648e5f8fe7675a75670e48ed
Kelsey H <kels-hall23@hotmail.com>
2021-12-22 15:06:49 -0500

    first commit, base structure added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+import todoList from './tempDatabase';
+import { TextField, Checkbox } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
 function App() {
+  // const [checked, setChecked] = React.useState(true);
+  // const handleChange = (event) => {
+  //   setChecked(event.target.checked);
+  // };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todo App</h1>
       </header>
+      <div>
+        <form>
+          <TextField
+            id="standard-basic"
+            label="Add New todo"
+            variant="standard"
+          />
+          <button>Add</button>
+        </form>
+      </div>
+      <ul>
+        {todoList.map(({ task, completed }) => (
+          <li key={task}>
+            <p>
+              {task}{' '}
+              <span style={{ fontWeight: 'bold' }}>
+                status:
+                {
+                  <Checkbox
+                    name={task}
+                    checked={completed}
+                    onChange={() => console.log('click')}
+                    inputProps={{ 'aria-label': { task } }}
+                  />
+                }
+                {completed ? 'complete' : 'incomplete'}
+              </span>
+              <button>
+                <EditIcon />{' '}
+              </button>
+              <button key={task} onClick={() => console.log(task)}>
+                X
+              </button>
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/tempDatabase.js
@@ -0,0 +1,20 @@
+const todoList = [
+  {
+    task: 'drink water',
+    completed: false,
+  },
+  {
+    task: 'walk zoey',
+    completed: true,
+  },
+  {
+    task: 'do groceries',
+    completed: false,
+  },
+  {
+    task: 'take nap',
+    completed: true,
+  },
+];
+
+export default todoList;
